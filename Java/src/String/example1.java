3787d38bbe380659ba17b8daa4524e92fbf7f714
ajith <ajith@AjithAj>
2023-11-01 11:30:29 +0530

    program example1

---
+++ Java/src/String/example1.java
@@ -0,0 +1,11 @@
+package String;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		System.out.println(30+40+"JavaString");
+		System.out.println("JavaString"+20+40);
+
+	}
+
+}
