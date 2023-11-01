41bd91fa8c82406c2c5f2e9401cbadef1b2aff06
ajith <ajith@AjithAj>
2023-11-01 11:45:59 +0530

    third example

---
+++ Java/src/String/example3.java
@@ -0,0 +1,12 @@
+package String;
+
+public class example3 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i<10; i++) {//        for(int i=0; 0; i++)   Compile time Error 
+			System.out.println(i+" Hello Javapoint");
+
+		}
+	}
+
+}
