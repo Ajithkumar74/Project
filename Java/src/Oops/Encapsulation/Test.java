00d6d8c6d675991f0fc86fb99b24af15444123a0
ajith <ajith@AjithAj>
2023-11-02 17:05:19 +0530

    Encapsulation

---
+++ Java/src/Oops/Encapsulation/Test.java
@@ -0,0 +1,13 @@
+package Oops.Encapsulation;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s=new Student();
+		s.setName("Vijay");
+		System.out.println(s.getName());
+	}
+
+}
