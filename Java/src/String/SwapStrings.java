ac13d6ac63522bad89e20c9b987b7171356fb8f2
ajith <ajith@AjithAj>
2023-11-08 12:54:07 +0530

    Swap String

---
+++ Java/src/String/SwapStrings.java
@@ -0,0 +1,14 @@
+package String;
+
+public class SwapStrings {
+
+	public static void main(String[] args) {
+		String str1="Good",str2="morning";
+		str1=str1+str2;
+		str2=str1.substring(0,(str1.length()-str2.length()));
+		str1=str1.substring(str2.length());
+	System.out.println("strings after swapping:"+str1+" "+str2);
+	}
+	
+
+}
