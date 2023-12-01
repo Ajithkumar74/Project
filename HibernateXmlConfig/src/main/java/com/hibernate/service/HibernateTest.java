ad9bf4c9c188b923cdfad26fa0ce2c9ddd1613f7
ajith <ajith@AjithAj>
2023-12-01 17:34:12 +0530

    Hibernate xml configurations

---
+++ HibernateXmlConfig/src/main/java/com/hibernate/service/HibernateTest.java
@@ -0,0 +1,25 @@
+package com.hibernate.service;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.beans.UserDetails;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			UserDetails user=new UserDetails();
+			user.setId(3);
+			user.setUsername("Third User");
+			SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
+					Session session=sessionFactory.openSession();
+					session.beginTransaction();
+					session.save(user);
+					
+					session.getTransaction().commit();
+
+			
+	}
+}
